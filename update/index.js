b6490b669a62585a8d86e36200c7b30fe3e6cd13
Matthias Jäggli <matthias.jaeggli@gmail.com>
2018-11-02 17:37:13 +0100

    Updating posts from wiki pages

--- update/index.js
+++ update/index.js
@@ -2,11 +2,17 @@ const path = require('path')
 const fs = require('fs-extra')
 const exec = require('./lib/exec')
 
-const tempFolder = path.join(__dirname, '.tmp')
+const cloneFolder = path.join(__dirname, '..', '_posts')
 
 const update = async () => {
-  await fs.remove(tempFolder)
-  await exec('git clone git@github.com:qualipool/swissrets.wiki.git wiki')
+  await fs.remove(cloneFolder)
+  await exec(
+    `git clone git@github.com:qualipool/swissrets.wiki.git ${cloneFolder}`,
+    { stdio: 'inherit' }
+  )
+  await exec('git add -A', { stdio: 'inherit' })
+  await exec('git commit -m "Updating posts from wiki pages"')
+  await exec('git push origin', { stdio: 'inherit' })
 }
 
 update()
