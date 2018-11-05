252af17a42b832f5eedadfa318e82595950cfead
Matthias Jäggli <matthias.jaeggli@gmail.com>
2018-11-05 07:50:38 +0100

    debugging

--- update/index.js
+++ update/index.js
@@ -2,21 +2,30 @@ const path = require('path')
 const fs = require('fs-extra')
 const exec = require('./lib/exec')
 
-const cloneFolder = path.join(__dirname, '..', '_posts')
+const destinationFolder = '_posts'
+const cloneFolder = path.join(__dirname, '..', destinationFolder)
 const indexFileSrc = path.join(cloneFolder, 'Home.md')
 const indexFileDest = path.join(cloneFolder, '..', 'index.md')
+
+const execConfig = {
+  stdio: 'inherit',
+  printCommand: true
+}
 
 const update = async () => {
   await fs.remove(cloneFolder)
   await exec(
-    `git clone git@github.com:qualipool/swissrets.wiki.git ${cloneFolder}`,
-    { stdio: 'inherit' }
+    `git clone --depth=1 git@github.com:qualipool/swissrets.wiki.git ${cloneFolder}`,
+    execConfig
     )
+  await fs.remove(path.join(cloneFolder, '.git'))
   await fs.remove(indexFileDest)
   await fs.move(indexFileSrc, indexFileDest)
-  await exec('git add -A', { stdio: 'inherit' })
+  await exec(`git add index.md`, execConfig)
+  await exec(`git rm --cached ${destinationFolder}/*`, execConfig)
+  await exec(`git add -A ${destinationFolder}/*`, execConfig)
   await exec('git commit -m "Updating posts from wiki pages"')
-  await exec('git push origin', { stdio: 'inherit' })
+  await exec('git push origin', execConfig)
 }
 
 update()
