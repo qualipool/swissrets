1770bd3c7672b87aff05f8c8f05e82df50592af9
stelan <stefan.landgraf@scout24.ch>
2018-12-03 17:49:46 +0100

    modified test files

--- tests/Schema_should.cs
+++ tests/Schema_should.cs
@@ -11,37 +11,49 @@ using FluentAssertions;
 
 namespace SwissRETS.Tests
 {
-  public class Schema_should
-  {
-    StringBuilder validationErrors = new StringBuilder();
-
-    [Fact]
-    public void be_valid_xml()
+    public class Schema_should
     {
-      var document = new XmlDocument();
-      document.Load("Schema/schema.xsd");
-    }
+        StringBuilder validationErrors = new StringBuilder();
 
-    [Fact]
-    public void validate_single_property_file()
-    {
-      var schema = new XmlSchemaSet();
-      schema.Add("", @"Schema/schema.xsd");
-      var rd = XmlReader.Create(@"TestData/SingleProperty.xml");
-      var doc = XDocument.Load(rd, LoadOptions.PreserveWhitespace | LoadOptions.SetLineInfo);
-      this.validationErrors.Clear();
-      doc.Validate(schema, ValidationEventHandler, true);
-      this.validationErrors.ToString().Should().BeNullOrEmpty(this.validationErrors.ToString());
-    }
+        [Fact]
+        public void be_valid_xml()
+        {
+            var document = new XmlDocument();
+            document.Load("Schema/schema.xsd");
+        }
 
-    #region ValidationEventHandler
-    void ValidationEventHandler(object sender, ValidationEventArgs e)
-    {
-      if (e.Severity == XmlSeverityType.Error)
-      {
-        this.validationErrors.AppendLine($"\n\n>> {e.Message}\n-> XML line {e.Exception.LineNumber} position {e.Exception.LinePosition}");
-      }
+        [Fact]
+        public void validate_full_file()
+        {
+            var schema = new XmlSchemaSet();
+            schema.Add("", @"Schema/schema.xsd");
+            var rd = XmlReader.Create(@"TestData/full.xml");
+            var doc = XDocument.Load(rd, LoadOptions.PreserveWhitespace | LoadOptions.SetLineInfo);
+            this.validationErrors.Clear();
+            doc.Validate(schema, ValidationEventHandler, true);
+            this.validationErrors.ToString().Should().BeNullOrEmpty(this.validationErrors.ToString());
+        }
+
+        [Fact]
+        public void validate_minmal_file()
+        {
+            var schema = new XmlSchemaSet();
+            schema.Add("", @"Schema/schema.xsd");
+            var rd = XmlReader.Create(@"TestData/minimal.xml");
+            var doc = XDocument.Load(rd, LoadOptions.PreserveWhitespace | LoadOptions.SetLineInfo);
+            this.validationErrors.Clear();
+            doc.Validate(schema, ValidationEventHandler, true);
+            this.validationErrors.ToString().Should().BeNullOrEmpty(this.validationErrors.ToString());
+        }
+
+        #region ValidationEventHandler
+        void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            if (e.Severity == XmlSeverityType.Error)
+            {
+                this.validationErrors.AppendLine($"\n\n>> {e.Message}\n-> XML line {e.Exception.LineNumber} position {e.Exception.LinePosition}");
+            }
+        }
+        #endregion
     }
-    #endregion
-  }
 }
