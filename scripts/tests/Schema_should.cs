5179f4e0222b6fec9acd5006773ca5cb0a76205a
Matthias Jäggli <matthias.jaeggli@gmail.com>
2018-12-04 17:21:02 +0100

    refactor for deduplication

--- scripts/tests/Schema_should.cs
+++ scripts/tests/Schema_should.cs
@@ -13,45 +13,49 @@ namespace SwissRETS.Tests
 {
     public class Schema_should
     {
+        string schemaFilename = @"Schema/schema.xsd";
+        string currentFilename = null;
         StringBuilder validationErrors = new StringBuilder();
 
         [Fact]
         public void be_valid_xml()
         {
             var document = new XmlDocument();
-            document.Load("Schema/schema.xsd");
+            document.Load(this.schemaFilename);
         }
 
         [Fact]
         public void validate_full_file()
         {
-            var schema = new XmlSchemaSet();
-            schema.Add("", @"Schema/schema.xsd");
-            var rd = XmlReader.Create(@"TestData/full.xml");
-            var doc = XDocument.Load(rd, LoadOptions.PreserveWhitespace | LoadOptions.SetLineInfo);
-            this.validationErrors.Clear();
-            doc.Validate(schema, ValidationEventHandler, true);
-            this.validationErrors.ToString().Should().BeNullOrEmpty(this.validationErrors.ToString());
+            this.validate(@"TestData/full.xml");
         }
 
         [Fact]
         public void validate_minmal_file()
         {
+            this.validate(@"TestData/minimal.xml");
+        }
+
+        #region validate
+        void validate (string filename)
+        {
             var schema = new XmlSchemaSet();
-            schema.Add("", @"Schema/schema.xsd");
-            var rd = XmlReader.Create(@"TestData/minimal.xml");
+            schema.Add("", this.schemaFilename);
+            var rd = XmlReader.Create(filename);
             var doc = XDocument.Load(rd, LoadOptions.PreserveWhitespace | LoadOptions.SetLineInfo);
             this.validationErrors.Clear();
+            this.currentFilename = filename;
             doc.Validate(schema, ValidationEventHandler, true);
             this.validationErrors.ToString().Should().BeNullOrEmpty(this.validationErrors.ToString());
         }
+        #endregion
 
         #region ValidationEventHandler
         void ValidationEventHandler(object sender, ValidationEventArgs e)
         {
             if (e.Severity == XmlSeverityType.Error)
             {
-                this.validationErrors.AppendLine($"\n\n>> {e.Message}\n-> XML line {e.Exception.LineNumber} position {e.Exception.LinePosition}");
+                this.validationErrors.AppendLine($"\n{this.currentFilename}:{e.Exception.LineNumber}:{e.Exception.LinePosition}\n> {e.Message}");
             }
         }
         #endregion
