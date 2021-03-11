2b9cdfd7dd4a114f694f728889720caa94935566
jensstalder <jens@stalderstudios.ch>
2021-03-11 19:34:12 +0100

    Set it to point to unitReference instead, and updated xmllint test to run synchonous

--- scripts/xmllint/index.js
+++ scripts/xmllint/index.js
@@ -5,62 +5,46 @@ const schemaPath = path.join(__dirname, '..', '..', 'schema', 'schema.xsd')
 const glob = require('glob')
 const { log } = require('../lib')
 
-fs.readFile(schemaPath, {}, (err, schema) => {
-  if (err) {
-    log.failure(err)
+const schema = fs.readFileSync(schemaPath, {})
+const shouldPassPath = path.join(__dirname, '..', '..', 'examples')
+const shouldPassFiles = glob.sync(path.join(shouldPassPath, '*.xml'))
+const shouldFailPath = path.join(__dirname, 'should-fail')
+const shouldFailFiles = glob.sync(path.join(shouldFailPath, '*.xml'))
+
+log.title(`Checking that Example files are Valid: ${shouldPassFiles.length}`)
+shouldPassFiles.forEach((file) => {
+  const xml = fs.readFileSync(file, {})
+  const result = xmllint.validateXML({
+    xml,
+    schema
+  })
+  if (!result.errors) {
+    log.success(`✅ Example XML passes: ${file}`)
+  } else {
+    log.failure(
+      `❌ There are validation errors in example file: ${file}`
+    )
+    result.errors.forEach((singleError) => {
+      log.failure(singleError)
+    })
     process.exit(1)
   }
-  const shouldPassPath = path.join(__dirname, '..', '..', 'examples')
-  glob(path.join(shouldPassPath, '*.xml'), function (er, files) {
-    files.forEach((file) => {
-      fs.readFile(file, {}, (err, xml) => {
-        if (err) {
-          log.failure(err)
-        } else {
-          const result = xmllint.validateXML({
-            xml,
-            schema
-          })
+})
 
-          if (!result.errors) {
-            log.success(`✅ Example XML passes: ${file}`)
-          } else {
-            log.failure(
-              `❌ There are validation errors in example file: ${file}`
-            )
-            result.errors.forEach((singleError) => {
-              log.failure(singleError)
-            })
-            process.exit(1)
-          }
-        }
-      })
-    })
+log.title(`Checking that non-valid "should-fail" xmls are detected as invalid: ${shouldFailFiles.length}`)
+shouldFailFiles.forEach((file) => {
+  const xml = fs.readFileSync(file, {})
+  const result = xmllint.validateXML({
+    xml,
+    schema
   })
-
-  const shouldFailsPath = path.join(__dirname, 'should-fail')
-  glob(path.join(shouldFailsPath, '*.xml'), function (er, files) {
-    files.forEach((file) => {
-      fs.readFile(file, {}, (err, xml) => {
-        if (err) {
-          log.failure(err)
-        } else {
-          const result = xmllint.validateXML({
-            xml,
-            schema
-          })
-
-          if (!result.errors) {
-            log.failure(`❌ File should have failed but it did not: ${file}`)
-            process.exit(1)
-          } else {
-            log.success(`✅ Errors found and expected for this file: ${file}`)
-            result.errors.forEach((singleError) => {
-              log.info(singleError)
-            })
-          }
-        }
-      })
+  if (!result.errors) {
+    log.failure(`❌ File should have failed but it did not: ${file}`)
+    process.exit(1)
+  } else {
+    log.success(`✅ Errors found and expected for this file: ${file}`)
+    result.errors.forEach((singleError) => {
+      log.info(singleError)
     })
-  })
+  }
 })
