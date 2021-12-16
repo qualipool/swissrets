fc34908334c1f56e72d6cc5d23c4a2b0449cc73b
jensstalder <jens@stalderstudios.ch>
2021-12-16 22:33:43 +0100

    phpcs and phpstan lvl9

--- scripts/php/run.php
+++ scripts/php/run.php
@@ -1,8 +1,29 @@
 <?php
 
+/**
+ * This file is intended for running schema validations.
+ * 
+ * The directory examples needs to pass and "should-fail" variants should fail.
+ * The script will exit with the appropriate code.
+ * 
+ * php version 8.2.0
+ *
+ * @category  Test
+ * @package   SwissRETS
+ * @author    Jens Stalder <jens@stalderstudios.ch>
+ * @copyright 2021 Vereinigung Qualipool
+ * @license   https://qualipool.ch/ MIT
+ * @link      https://github.com/qualipool/swissrets
+ */
+
 libxml_use_internal_errors(true);
 
-function run()
+/**
+ * Main run function
+ * 
+ * @return void
+ */
+function run(): void
 {
     $schemaFile = '../../schema/schema.xsd';
 
@@ -10,27 +31,36 @@ function run()
     $positiveSuccesses = 0;
     $exampleFilesCount = 0;
     $exampleFiles = scandir('../../examples');
-    foreach ($exampleFiles as $exampleFile) {
-        if(strpos($exampleFile, '.xml')) {
-            $exampleFilesCount++;
-            $success = shouldPass('../../examples/' . $exampleFile, $schemaFile);
-            if ($success) {
-                $positiveSuccesses++;
+    if ($exampleFiles !== false) {
+        foreach ($exampleFiles as $exampleFile) {
+            if (strpos($exampleFile, '.xml')) {
+                $exampleFilesCount++;
+                $success = shouldPass('../../examples/' . $exampleFile, $schemaFile);
+                if ($success) {
+                    $positiveSuccesses++;
+                }
             }
         }
     }
     print "\n\n";
 
-    print consoleTitle("Checking that non-valid \"should-fail\" xmls are detected as invalid:\n");
+    print consoleTitle(
+        "Checking that non-valid \"should-fail\" xmls are detected as invalid:\n"
+    );
     $positiveFailures = 0;
     $shouldFailFilesCount = 0;
     $shouldFailFiles = scandir('../../scripts/xmllint/should-fail');
-    foreach ($shouldFailFiles as $shouldFailFile) {
-        if(strpos($shouldFailFile, '.xml')) {
-            $shouldFailFilesCount++;
-            $success = shouldFail('../../scripts/xmllint/should-fail/' . $shouldFailFile, $schemaFile);
-            if ($success) {
-                $positiveFailures++;
+    if ($shouldFailFiles !== false) {
+        foreach ($shouldFailFiles as $shouldFailFile) {
+            if (strpos($shouldFailFile, '.xml')) {
+                $shouldFailFilesCount++;
+                $success = shouldFail(
+                    '../../scripts/xmllint/should-fail/' . $shouldFailFile,
+                    $schemaFile
+                );
+                if ($success) {
+                    $positiveFailures++;
+                }
             }
         }
     }
@@ -57,84 +87,152 @@ function run()
     }
 }
 
-function consoleCheckmark()
+/**
+ * Adds check mark and formatting for console.
+ * 
+ * @return string
+ */
+function consoleCheckmark(): string
 {
     return consoleReallyGood("✓") . consoleReset();
 }
 
-function consoleXmark()
+/**
+ * Adds X mark and formatting for console.
+ * 
+ * @return string
+ */
+function consoleXmark(): string
 {
     return consoleReallyBad("𐄂") . consoleReset();
 }
 
-function consoleTitle($str)
+/**
+ * Adds styling for console.
+ * 
+ * @param string $str the string that needs to be formatted
+ * 
+ * @return string
+ */
+function consoleTitle(string $str): string
 {
     return "\e[1m" . $str . consoleReset();
 }
 
-function consoleBad($str)
+/**
+ * Adds styling for console.
+ * 
+ * @param string $str the string that needs to be formatted
+ * 
+ * @return string
+ */
+function consoleBad(string $str): string
 {
     return "\e[31m" . $str . consoleReset();
 }
 
-function consoleReallyBad($str)
+/**
+ * Adds styling for console.
+ * 
+ * @param string $str the string that needs to be formatted
+ * 
+ * @return string
+ */
+function consoleReallyBad(string $str): string
 {
     return "\e[30;48;5;1m" . $str . consoleReset();
 }
 
-function consoleReallyGood($str)
+/**
+ * Adds styling for console.
+ * 
+ * @param string $str the string that needs to be formatted
+ * 
+ * @return string
+ */
+function consoleReallyGood(string $str): string
 {
     return "\e[30;48;5;82m" . $str . consoleReset();
 }
 
-function consoleReset()
+/**
+ * Adds styling for console.
+ * 
+ * @return string
+ */
+function consoleReset(): string
 {
     return "\e(B\e[m";
 }
 
-function shouldPass(string $xmlFile, string $schemaFile)
+/**
+ * Tests if xml file is valid with provided schema.
+ * prints directly to the console
+ * 
+ * @param string $xmlFile    path to xml file to test
+ * @param string $schemaFile path to schema to test against
+ * 
+ * @return bool
+ */
+function shouldPass(string $xmlFile, string $schemaFile): bool
 {
     $xml = new DOMDocument();
     $xml->load($xmlFile);
     if (!$xml->schemaValidate($schemaFile)) {
         print "\n";
-        print consoleXmark() . ' ' . consoleTitle($xmlFile);"\n";  
+        print consoleXmark() . ' ' . consoleTitle($xmlFile);
         $errors = libxml_get_errors();
         foreach ($errors as $error) {
             print "\n";
-            print ' | ' . libxml_display_error($error);
+            print ' | ' . libxmlDisplayError($error);
         }
         libxml_clear_errors();
         return false;
     }
     print "\n";
-    print consoleCheckmark() . ' ' . consoleTitle($xmlFile);"\n";  
+    print consoleCheckmark() . ' ' . consoleTitle($xmlFile);
     
     return true;
 }
 
-function shouldFail(string $xmlFile, string $schemaFile)
+/**
+ * Tests if xml file is failing with provided schema.
+ * prints directly to the console
+ * 
+ * @param string $xmlFile    path to xml file to test
+ * @param string $schemaFile path to schema to test against
+ * 
+ * @return bool
+ */
+function shouldFail(string $xmlFile, string $schemaFile): bool
 {
     $xml = new DOMDocument();
     $xml->load($xmlFile);
     if (!$xml->schemaValidate($schemaFile)) {
         print "\n";
-        print consoleCheckmark() . ' ' . consoleTitle($xmlFile);"\n";  
+        print consoleCheckmark() . ' ' . consoleTitle($xmlFile);
         $errors = libxml_get_errors();
         foreach ($errors as $error) {
             print "\n";
-            print ' | ' . libxml_display_error($error);
+            print ' | ' . libxmlDisplayError($error);
         }
         libxml_clear_errors();
         return true;
     }
     print "\n";
-    print consoleXmark() . ' ' . consoleTitle($xmlFile);"\n";  
+    print consoleXmark() . ' ' . consoleTitle($xmlFile);
     
     return false;
 }
 
-function libxml_display_error($error)
+/**
+ * Converts a libxml error to a sting (with console formatting)
+ * 
+ * @param \LibXMLError $error will be formatted to a console string
+ * 
+ * @return string
+ */
+function libxmlDisplayError($error): string
 {
     $return = '';
     switch ($error->level) {
@@ -152,7 +250,7 @@ function libxml_display_error($error)
     if ($error->file) {
         $return .= consoleBad(" " . $error->file);
     }
-    $return .= " on line " . consoleBad($error->line);
+    $return .= " on line " . consoleBad((string) $error->line);
 
     return $return;
 }
